449cb5af230905f90a19f1b66ec3553694deba12
why are you <bionixxxd5@gmail.com>
2024-07-27 12:40:56 +0300

    feat: added hook useMediaDevice prototype

--- src/components/ui/playerSettings.tsx
+++ src/components/ui/playerSettings.tsx
@@ -1,9 +1,68 @@
 "use client";
 
+import { useMediaDevice } from "@/utils";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 export const PlayerSettings = () => {
+  const { webcams, microphones, outputDevices, videoWrapperRef, changeCamera, changeAudio } = useMediaDevice();
+  console.log("webcams", webcams[0]?.label);
+  console.log("microphones", microphones);
+  const onChangeCamera = (value: string) => {
+    console.log(value);
+    changeCamera(value);
+  };
+  const onChangeAudio = (value: string) => {
+    console.log(value);
+    changeAudio(value);
+  };
   return (
     <div>
       <h2>123321</h2>
+      <video ref={videoWrapperRef} className="w-[500px] h-[500px]" autoPlay />
+      <Select onValueChange={onChangeCamera}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Источник видео" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {webcams.map((webcam) => (
+              <SelectItem value={webcam?.deviceId}>{webcam?.label}</SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Select onValueChange={onChangeAudio}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Микрофон" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {microphones.map((microphone) => (
+              <SelectItem value={microphone?.deviceId}>{microphone?.label}</SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Выход звука" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {outputDevices.map((outputDevice) => (
+              <SelectItem value={outputDevice?.deviceId}>{outputDevice?.label}</SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
     </div>
   );
 };
--- src/utils/hooks/useMediaDevice.ts
+++ src/utils/hooks/useMediaDevice.ts
@@ -1,5 +1,90 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
+
+const constraints = {
+  video: true,
+  audio: true,
+};
 export const useMediaDevice = () => {
-  return 1;
+  const [microphones, setMicrophones] = useState<MediaDeviceInfo[]>([]);
+  const [webcams, setWebcams] = useState<MediaDeviceInfo[]>([]);
+  const [outputDevices, setOutputDevices] = useState<MediaDeviceInfo[]>([]);
+
+  const videoWrapperRef = useRef(null);
+
+  const getConnectedDevices = async () => {
+    const devices = await navigator.mediaDevices
+      .enumerateDevices()
+      .then((devices) => {
+        setMicrophones(devices.filter((device) => device.kind === "audioinput"));
+        setWebcams(devices.filter((device) => device.kind === "videoinput"));
+        setOutputDevices(devices.filter((device) => device.kind === "audiooutput"));
+      })
+      .catch((err) => console.log(err));
+    return devices;
+  };
+
+  async function playVideoFromCamera() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      if (videoWrapperRef) {
+        videoWrapperRef.current.srcObject = stream;
+      }
+    } catch (error) {
+      console.error("Error opening video camera.", error);
+    }
+  }
+
+  const changeCamera = async (cameraId: string) => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          deviceId: cameraId,
+        },
+      });
+      console.log("stream", stream);
+      if (videoWrapperRef) {
+        videoWrapperRef.current.srcObject = stream;
+      }
+    } catch (error) {
+      console.error("Error opening video camera.", error);
+    }
+  };
+  const changeAudio = async (audioId: string) => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: {
+          deviceId: audioId,
+        },
+      });
+      console.log("stream", stream);
+      if (videoWrapperRef) {
+        videoWrapperRef.current.srcObject = stream;
+      }
+    } catch (error) {
+      console.error("Error opening video camera.", error);
+    }
+  };
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(() => {
+        getConnectedDevices();
+        playVideoFromCamera();
+      })
+      .catch((error) => {
+        console.error("Error accessing media devices.", error);
+      });
+  }, [videoWrapperRef]);
+  console.log("videoWrapperRef", videoWrapperRef);
+  return {
+    microphones,
+    webcams,
+    outputDevices,
+    videoWrapperRef,
+    changeCamera,
+    changeAudio,
+  };
 };
